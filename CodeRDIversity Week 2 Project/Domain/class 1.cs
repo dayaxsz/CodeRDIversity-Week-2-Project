84b47ac4eef12856b3f8508aca26aac5d1d224ce
tsuukisz <carolmagalhaes.lima@gmail.com>
2024-08-16 19:41:53 -0300

    Add project files.

---
+++ CodeRDIversity Week 2 Project/Domain/class 1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeRDIversity_Week_2_Project.Domain
+{
+    public abstract class 
+    {
+
+    }
+}
---
+++ CodeRDIversity Week 2 Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeRDIversity_Week_2_Project.Domain
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+
+
+}
